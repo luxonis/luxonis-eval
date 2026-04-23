76fd36eab255385978eb0be07707e6b58df66909
Dani Rogmans <danirogmans@gmail.com>
2026-04-23 14:28:02 +0200

    remove unified

--- luxonis_eval/parsers/__init__.py
+++ luxonis_eval/parsers/__init__.py
@@ -4,7 +4,6 @@ from .detection import YOLODetectionParser
 from .instance_seg import YOLOInstanceSegmentationParser
 from .keypoint_detection import YOLOKeypointDetectionParser
 from .semantic_seg import SemanticSegmentationParser
-from .unified_instance import YOLOUnifiedInstanceParser
 
 __all__ = [
     "BaseParser",
